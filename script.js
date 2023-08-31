8ec5b9a3f91f1db24510cd449c51fdb84493965b
tomdalby <thomas.b.dalby@gmail.com>
2023-08-31 10:38:41 +0200

    made header smaller. Made menu un-scrollable. Moved copy right text higher in menu.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const burgerMenu = document.getElementById("burger-menu");
   const mobileMenu = document.getElementById("mobile-menu");
+  const body = document.querySelector("body");
 
   burgerMenu.addEventListener("click", function () {
     if (mobileMenu.classList.contains("active")) {
       mobileMenu.classList.remove("active");
     } else {
       mobileMenu.classList.add("active");
+      body.style.overflow = "hidden";
     }
   });
 });
