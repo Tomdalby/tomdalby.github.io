a549a61c47bc78ab71677ca4a45f8fbb4f8c89ac
Tom Dalby <tomdalby@Mac.fritz.box>
2026-05-12 15:48:55 +0200

    Restructure project for Vite build (move components and index to root)

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,23 +3,25 @@ import { resolve } from "path";
 import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
+  base: "/tomdalby.github.io/",
+
   plugins: [injectHTML()],
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "index.html"),
-      about: resolve(__dirname, "src/pages/about.html"),
-      music: resolve(__dirname, "src/pages/music.html"),
-      spaces: resolve(__dirname, "src/pages/spaces.html"),
-    },
-    output: {
-      assetFileNames: "assets/[name].[hash][extname]",
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+       about: resolve(__dirname, "src/pages/about.html"),
+music: resolve(__dirname, "src/pages/music.html"),
+spaces: resolve(__dirname, "src/pages/spaces.html"),
+      },
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+      },
     },
   },
 
-  root: "src",
-  build: {
-    outDir: "dist",
-  },
   server: {
     port: 3000,
     open: true,
